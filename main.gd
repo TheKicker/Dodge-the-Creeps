dd2ea227f41636fc59f75d76f3d429b76a2fa5ea
The Kicker <cav.lemasters@geneva.edu>
2022-01-27 15:17:12 -0500

    Game Complete :sparkles: :art: :zap:

--- main.gd
+++ main.gd
@@ -5,16 +5,22 @@ var score
 
 func _ready():
 	randomize()
-	new_game()
 
 
 func game_over():
 	$ScoreTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	
 func new_game():
 	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 
 func _on_MobTimer_timeout():
@@ -43,6 +49,7 @@ func _on_MobTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_StartTimer_timeout():
